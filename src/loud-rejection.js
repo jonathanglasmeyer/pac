06254b7ff9d9881972b4f6cc57bce18254028aa9
Jonathan Werner <jwerner@posteo.de>
2016-03-13 15:13:50 +0100

    log stack for unhandledrejection

--- src/loud-rejection.js
+++ src/loud-rejection.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 window.onload = function() {
-  process.on('unhandledRejection', (reason, p) => {
-      console.log("Unhandled Rejection at: Promise ", p, " reason: ", reason);
+  process.on('unhandledRejection', (err, p) => {
+      console.log("Unhandled Rejection at: Promise ", p, " err: ", err);
+      console.error(err.stack || err);
   });
 }
